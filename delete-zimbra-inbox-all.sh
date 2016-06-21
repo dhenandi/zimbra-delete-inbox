f6bdf5ff3d4ab3f93c30be130f5a347ee8cfbb11
dhenandi <muhammad@dhenandi.com>
2016-06-21 22:22:06 +0700

    Update delete-zimbra-inbox-all.sh

--- delete-zimbra-inbox-all.sh
+++ delete-zimbra-inbox-all.sh
@@ -7,13 +7,13 @@ echo "Deleting Inbox. Please Wait..."
 LOCATION=`pwd`
 USERS=`su - zimbra -c "zmprov -l gaa | sort"`
 
-echo "Looping for all users"
 for ACCOUNT in $USERS; do
         ACC1=`echo $ACCOUNT | awk -F@ '{print $1}'`;
         ACC2=`echo $ACCOUNT | cut -d '.' -f1`
  
 #Delete Inbox
         su - zimbra -c "zmmailbox -z -m $ACCOUNT emptyFolder /Inbox"
-        echo "Inbox dari email Account $ACCOUNT telah selesai"
+        echo "$ACCOUNT Inbox has been deleted"
 done
-echo "Proses sinkronisasi email selesai dilakukan"
+echo ""
+echo "All Inbox Has Been Deleted"
